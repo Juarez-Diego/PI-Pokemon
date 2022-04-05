4ed49f743b4f1756734ab752b7c7f25d99c5e12c
Juarez-Diego <diegoalbertojuarez@yahoo.com>
2022-04-05 10:08:40 -0500

    Created models

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -1,12 +1,38 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes,Sequelize } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('pokemon', {
+  sequelize.define('Pokemon', {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    hp: {
+      type: DataTypes.DECIMAL
+    },
+    attack: {
+      type: DataTypes.DECIMAL
+    },
+    defense: {
+      type: DataTypes.DECIMAL
+    },
+    speed: {
+      type: DataTypes.DECIMAL
+    },
+    height: {
+      type: DataTypes.DECIMAL
+    },
+    weight: {
+      type: DataTypes.DECIMAL
+    },
+
+
   });
 };
